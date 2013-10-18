bc4a4c711bf02c23b924e60cec63917e79aef820
Jared Prather <pratherjared@gmail.com>
2013-10-18 13:36:24 -0500

    Index code cleanup

---
+++ www/js/input.js
@@ -0,0 +1,14 @@
+﻿var element = document.getElementById('content');
+var hammertime = Hammer(element).on("swiperight", function (event) {
+    debugNote('Hammer: Swiperight event received');
+    event.preventDefault();
+    event.gesture.preventDefault();
+    openNav();
+});
+
+var hammertime = Hammer(element).on("swipeleft", function (event) {
+    debugNote('Hammer: Swipeleft event received');
+    event.preventDefault();
+    event.gesture.preventDefault();
+    closeNav();
+});
