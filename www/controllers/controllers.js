90bc9192788242f3203580b32b6a0cee80270f2c
Jared Prather <pratherjared@gmail.com>
2013-10-21 19:24:52 -0500

    Updated homepage and more alert UI changes

--- www/controllers/controllers.js
+++ www/controllers/controllers.js
@@ -55,7 +55,7 @@ function MainCtrl($scope, $location, $rootScope, $http) {
         debugNote('EVENT: Page_title_change event received: ' + title);
     });
 
-    //Handle alert received event
+    //Handle alert received event globally
     $scope.$on("ALERT_RECEIVED", function (event, count) {
         $scope.newAlerts += count;
         debugNote('EVENT: Alert_received event received: ' + count);
@@ -306,11 +306,19 @@ function SettingsCtrl($scope, $http, $location) {
 function HomeCtrl($scope, AlertRestangular, $location) {
     $scope.authenticateUser();
     $scope.$emit("PAGE_TITLE_CHANGE", "ArrowAlert");
-
+    $scope.loading = true;
     //Fetch recent objects from the backend (see models/Alert.js)
     //Parameter age requests all alerts within the last 24 hours
     $scope.recentAlerts = AlertRestangular.all('Alerts').getList({ age: '24' });
     $scope.loading = false;
+
+
+    //Handle alert received event while on homescreen
+    $scope.$on("ALERT_RECEIVED", function (event, count) {
+        $scope.recentAlerts = AlertRestangular.all('Alerts').getList({ age: '24' });
+        debugNote('EVENT: Alert_received event received on homepage');
+    });
+
 }
 
 function AlertCtrl($scope, AlertRestangular) {
