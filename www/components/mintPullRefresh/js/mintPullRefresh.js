ab4d757d968acfe84486f70f8bf019e627612d30
Jared Prather <pratherjared@gmail.com>
2013-10-23 21:45:50 -0500

    Slightly cleaned up mintPullRefresh

--- www/components/mintPullRefresh/js/mintPullRefresh.js
+++ www/components/mintPullRefresh/js/mintPullRefresh.js
@@ -1,26 +1,33 @@
+//TODO: Organize/clean this up
 var pullingToRefreshSuccess = false;
 var showingTooltip = false;
 var mintPullInfoTooltip = document.getElementById('mintPullInfo');
+
 var pullToRefreshEnabled = false;
 
+//Gives user visual feedback on percent needed to pull
 function displayPullingQue(percent) {
     var element = document.getElementById('mintPullRefreshQue');
     var windowWidth = window.innerWidth;
     element.style.left = 50 - (50 * percent) + '%';
     element.style.right = 50 - (50 * percent) + '%';
 }
-//
+
 Hammer(document.getElementById('content')).on("drag", function (event) {
+    //Check if enabled
     if (pullToRefreshEnabled) {
+        //Check if refresh already achieved during this drag
         if (!pullingToRefreshSuccess) {
             if (event.gesture != null && event.gesture != 'undefined' && event.gesture.deltaY != 'undefined')
                 if (event.gesture.direction == "down" && document.getElementById('content').scrollTop == 0) {
+
                     prevention(event);
                     if (!showingTooltip) {
                         showTooltip();
                     }
-                    displayPullingQue(event.gesture.deltaY / 200);
 
+                    displayPullingQue(event.gesture.deltaY / 200);
+                    //If user pulls 200px down, send refresh command
                     if (event.gesture.deltaY >= 200) {
                         pullingToRefreshSuccess = true;
                         mintNotify.success("Refreshing!");
@@ -40,10 +47,6 @@ Hammer(document.getElementById('content')).on("dragend", function (event) {
         pullingToRefreshSuccess = false;
         displayPullingQue(0);
         hideTooltip();
-        //broadcastAngularEvent('DEBUG_SETTING_CHANGE', true);
-        //prevention(event);
-        ////debug log
-        //debugNote('INPUT: Swiperight event received on debugSwitch');
     }
 });
 
